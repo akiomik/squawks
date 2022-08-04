50aa3394edf155ca33e31f724be99aa158ec1f6a
Akiomi Kamakura <akiomik@gmail.com>
2022-08-04 14:54:40 +0900

    Add test for Version

---
+++ config/version_test.go
@@ -0,0 +1,31 @@
+// Copyright 2022 Akiomi Kamakura
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build small
+// +build small
+
+package config
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestVersion(t *testing.T) {
+	// from https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+	r := regexp.MustCompile(`^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`)
+	if !r.MatchString(Version) {
+		t.Errorf("Expect Version to match semver, but it does not")
+	}
+}
